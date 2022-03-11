841de77c93d063c68a6a29fdf5edf0709a31278b
Natalie <nataliemica@gmail.com>
2022-03-11 16:47:23 +0000

    adding lines to create figures and dataframes

--- JV_fitting_script.py
+++ JV_fitting_script.py
@@ -7,10 +7,13 @@ Created on Fri Mar 11 15:22:38 2022
 
 import pandas as pd
 import pathlib
+import dataframe_image as dfi
 
 import numpy as np
 
 import scipy.optimize as opt
+
+import matplotlib.pyplot as plt
 
 #find the data we want to fit, organize the dataframe
 data = pd.read_table(str(pathlib.Path().resolve())+'/data/example.txt', delimiter='\t', lineterminator='\n')
@@ -26,6 +29,22 @@ JV = JV.drop('Pixel', axis=1)
 
 #define ideal diode equation
 J = lambda V, Jsc, A, Jd : Jd*(np.exp(A*V)-1)-Jsc
+
+#fitting the first column
+my_fit, params = opt.curve_fit(J, JV.index.to_list(), JV['1'].values)
+fit1 = pd.DataFrame({'1':J(JV.index, my_fit[0], my_fit[1], my_fit[2])}, index=JV.index.to_list())
+
+plt.figure(figsize=(8,6)).set_facecolor('white')
+plt.plot(JV['1'], 'o', label='raw data')
+plt.plot(fit1['1'], color='darkred', label='fit')
+plt.xlabel('Voltage (V)', fontsize=15)
+plt.xticks(fontsize=12)
+plt.ylabel('Current Density (mA/cm2)', fontsize=15)
+plt.yticks(fontsize=12)
+plt.grid(axis='both')
+plt.legend(fontsize=12)
+plt.savefig('JVfit.png')
+plt.show()
 
 #set up characteristics dataframe
 chars_index = ['PCE (%)','Jsc (mA/cm2)','Voc (V)','FF (%)']
@@ -51,4 +70,11 @@ for col in JV.columns.to_list():
     #write the results to the appropriate column in the characteristics frame
     chars[col] = [PCE,JSC,VOC,FF]
     
-print(chars)
+dfi.export(chars,'chars.png')
+
+#taking a ratio of the fitted values to the expected values
+char_ratio = pd.DataFrame(chars.values/data[['1','2','3']].head()[1:5].values, 
+                         index=['PCE','Jsc','Voc','FF'],
+                         columns=['Pixel 1', 'Pixel 2', 'Piexl 3'])
+dfi.export(char_ratio,'char_ratio.png')
+char_ratio
